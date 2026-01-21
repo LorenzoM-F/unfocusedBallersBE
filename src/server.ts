6f73c19a9b5bedbc3ae1b7e3326ea2edc58f6142
lorenzofransman <lorenzo.fransman@outlook.com>
2026-01-21 15:15:30 +0200

    update port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import { config } from "./config";
 
-app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`);
+app.listen(config.port, "127.0.0.1", () => {
+  console.log(`API listening on ${config.port}`);
 });
